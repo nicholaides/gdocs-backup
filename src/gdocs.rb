3718475743d48fc287090f27d92cf6cd1b33797f
Mike Nicholaides <mike@thehypemachine.(none)>
2009-03-10 22:12:16 -0400

    Got it working again... damn net http

--- src/gdocs.rb
+++ src/gdocs.rb
@@ -17,6 +17,16 @@ class GDocs
     @auth_token = @service.getAuthTokenFactory.getAuthToken.getAuthorizationHeader(@list_url, "GET")
   end
   
+  def test
+    url = URI.parse "http://docs.google.com/feeds/download/documents/Export?docID=df3c8tkp_16cw3wv4&exportFormat=doc"
+    res = Net::HTTP.start(url.host) {|http|
+      http.get("/feeds/download/documents/Export?docID=df3c8tkp_16cw3wv4&exportFormat=doc", {'Authorization' => @auth_token, 'Accept' => '*/*' })
+    }
+    p url.path
+    p res
+    puts res.body
+  end
+  
   def backup
     feed = @service.getFeed(@list_url, DocumentListFeed.java_class, nil)
 
@@ -30,68 +40,58 @@ class GDocs
         h[:authors]      = entry.authors.map{|a| "#{a.name} <#{a.email}>" }.join(', ')
       end
     end
+    
+    files.reject!{|file| file[:type] != 'document' }
   
-  
+    gdocs_dir  = File.join(ENV["HOME"], 'gdocs-backups')
+    backup_dir = File.join(gdocs_dir, Time.now.to_i.to_s)
+    Dir.mkdir(gdocs_dir)  unless File.exist? gdocs_dir
+    Dir.mkdir(backup_dir) unless File.exist? backup_dir
+    
     @downloading_items = [true]*files.size
     
+    puts @auth_token
+    
     files.each_with_index do |file, index|
-      format = case file[:type]
-      when 'document'
-        "docID=#{file[:id]}&exportFormat=doc"
-      when 'presentation'
-        "docID=#{file[:id]}&exportFormat=ppt"
-      when 'spreadsheet'
-        "key=#{file[:id]}&fmcmd=4"
+      case file[:type]
+        when 'document'
+          file[:download_path]      = "/feeds/download/documents/Export?docID=#{file[:id]}&exportFormat=doc"
+          file[:download_host]      = 'docs.google.com'
+          file[:download_extension] = 'doc'
+        when 'presentation'
+          file[:download_path]      = "/feeds/download/presentations/Export?docID=#{file[:id]}&exportFormat=ppt"
+          file[:download_host]      = 'docs.google.com'
+          file[:download_extension] = 'ppt'
+        when 'spreadsheet'
+          file[:download_path]      = "/feeds/download/spreadsheets/Export?key=#{file[:id]}&fmcmd=4"
+          file[:download_host]      = 'spreadsheets.google.com'
+          file[:download_extension] = 'xls'
       end
-      path = "/feeds/download/#{file[:type]}s/Export?#{format}"
       
       semaphore = Mutex.new
       
-      Thread.start(index, files, path) do |index, files, path|
-        res = Net::HTTP.start("docs.google.com") {|http|
-          http.get(path, {'Authorization' => @auth_token})
+      Thread.start(index, files, backup_dir) do |index, files, backup_dir|
+        puts file[:download_path]
+        file = files[index]
+        res = Net::HTTP.start(file[:download_host]) {|http|
+          http.get(file[:download_path], {'Authorization' => @auth_token})
         }
+        puts res
+        file = files[index]
+        File.open( File.join(backup_dir, "#{file[:id]} - #{file[:title]}.#{file[:download_extension]}"), 'w' ) do |f|
+          f.print res.body
+        end
         
         semaphore.synchronize do
           @downloading_items[index] = false
-          puts "completed #{ @downloading_items.select{|i| i }.size }/#{@downloading_items.size}"
+          STDERR.puts "completed #{ @downloading_items.reject{|i| i }.size }/#{@downloading_items.size}"
+          STDERR.puts "waiting for: "
+          @downloading_items.each_with_index{|item, index|
+            p files[index][:title] if item
+          }
           puts "All complete!" if @downloading_items.none?
         end
       end
     end
-  end
-  
-  def stuff
-    list_url = java.net.URL.new("http://docs.google.com/feeds/documents/private/full")
-    
-
-    feed = service.getFeed(list_url, DocumentListFeed.java_class, nil)
-    entries = feed.entries
-    for entry in entries
-      puts entry.title.text
-      puts Time.at(entry.last_viewed.value) if entry.last_viewed
-      puts entry.can_edit
-      puts entry.categories.map{|c| c.label }.join(', ')
-      puts entry.authors.map{|a| "#{a.name} <#{a.email}>" }.join(', ')
-      puts entry.self_link.href
-      puts entry.html_link.href
-      puts 
-      puts
-    end
-    
-    auth_token = service.getAuthTokenFactory.getAuthToken.getAuthorizationHeader(list_url, "GET")
-
-
-    require 'net/http'
-    require 'uri'
-
-    id = entries[1].resource_id.gsub(/^.*\:/, '')
-
-    url = URI.parse('http://docs.google.com')
-    file_path = "/feeds/download/documents/Export?docID=#{id}&exportFormat=html" 
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.get("/feeds/documents/private/full", {'Authorization' => auth_token})
-    }
-    puts res.body
   end
 end
