1fa1d9ffd0f8ed3788399cb886d8e434b4fa9f3d
Mike Nicholaides <mike@thehypemachine.(none)>
2009-03-11 22:02:50 -0400

    Shows the file titles

--- src/backupfile.rb
+++ src/backupfile.rb
@@ -5,12 +5,17 @@ module GDocsBackup
     def initialize(file_name)
       @file_name = file_name
       
-      @id = File.basename(file_name).split(' - ').first
+      @id = File.basename(file_name).split(' - ').first 
       
       @yaml_file = File.join(File.dirname(@file_name), "#{@id}.yml")
       @attributes = YAML.load(File.new(@yaml_file))
     end
     
+    def to_s
+      title
+    end
+    
+    #proxy the attributes hash
     def method_missing(method, *args)
       if method.to_s =~ /=$/
         @attributes[ method.to_s.gsub(/=$/, '').to_sym ] = args
